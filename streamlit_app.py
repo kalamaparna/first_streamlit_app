240e085f5bcb29436272e8051ae03a2865749256
kalamaparna <121286176+kalamaparna@users.noreply.github.com>
2022-12-23 15:37:20 +0530

    Update streamlit_app.py
    new list added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('reakfast menu')
+streamlit.header('Breakfast menu')
 streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 kale,Spinach &Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range-Egg')
-streamlit.text('🥑🍞 Avocado Toast'
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
